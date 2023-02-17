218a0212fac8e8f947bee4baa28f52bb25f68aec
minthai222@gmail.com <you@example.com>
2023-02-17 14:38:18 +0700

    update main

--- BMIApp/src/main/java/com/tnmt/bmiapp/PrimaryController.java
+++ BMIApp/src/main/java/com/tnmt/bmiapp/PrimaryController.java
@@ -7,6 +7,8 @@ public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        int a = 10;
+        if (a > 0 && a % 2 == 0)
+            App.setRoot("secondary");
     }
 }
